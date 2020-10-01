470a13eb43448e99461dacaca31905482653ae88
Guilherme Rigotti <ggrigotti@gmail.com>
2020-10-01 15:21:49 -0300

    feat: remove autocomplete input

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -6,11 +6,17 @@ type Props = React.DetailedHTMLProps<
   HTMLInputElement
 >
 
-const Input: React.FC<Props> = (props: Props) => (
-  <div className={Styles.inputWrap}>
-    <input {...props} />
-    <span className={Styles.status}>🔴</span>
-  </div>
-)
+const Input: React.FC<Props> = (props: Props) => {
+  const enableInput = (event: React.FocusEvent<HTMLInputElement>): void => {
+    event.target.readOnly = false
+  }
+
+  return (
+    <div className={Styles.inputWrap}>
+      <input {...props} readOnly onFocus={enableInput} />
+      <span className={Styles.status}>🔴</span>
+    </div>
+  )
+}
 
 export default Input
