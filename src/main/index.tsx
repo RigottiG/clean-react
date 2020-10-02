90d4e46584b32c8ac33add73ae923cfbebba7d0c
Guilherme Rigotti <ggrigotti@gmail.com>
2020-10-02 15:51:22 -0300

    fix: bug gobal

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router } from '@/presentation/components/'
+import '@/presentation/styles/global.scss'
 
 ReactDOM.render(<Router />, document.getElementById('main'))
